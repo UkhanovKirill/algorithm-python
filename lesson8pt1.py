6d5f1951bf4d2de52df5c390a7892cc2ac911dac
UkhanovKirill <ukhanovkirill1@gmail.com>
2021-08-11 17:30:18 +0300

    lesson8

---
+++ lesson8pt1.py
@@ -0,0 +1,17 @@
+s = 'hello'
+
+def search_strings(s):
+    n = len(s)
+    arr_str = set()
+    for i in range(1, n):
+
+        for j in range(n - i + 1):
+
+            k = hash(s[j:j+i])
+
+            if k not in arr_str:
+                arr_str.add(k)
+
+    return len(arr_str)
+
+print(f'Количество подстрок: {search_strings(s)}')
